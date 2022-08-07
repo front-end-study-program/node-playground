e349a711356da5a03c60ca50c6baf967ce365ec8
yanquanfahei <1449826851@qq.com>
2022-08-07 15:13:43 +0800

    feat: error handing

---
+++ express/error-handing/app.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const createError = require('http-errors')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+app.get('/error', (req, res, next) => {
+  setTimeout(() => {
+    try {
+      throw new Error('BROKEN')
+    } catch (err) {
+      next(err)
+    }
+  }, 100)
+})
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  // res.render('error');
+  res.send(err.status || 'error')
+});
+
+app.listen(3000)
