1474ef545a81b715ff05c49bc017ffe85ab0126b
yanquanfahei <1449826851@qq.com>
2022-08-07 11:31:27 +0800

    fix: static path

--- express/static-files/app.js
+++ express/static-files/app.js
@@ -3,12 +3,12 @@ const path = require('path')
 
 const app = express()
 
+console.log('111', path.resolve(__dirname + '/public'))
+app.use(express.static(path.resolve(__dirname + '/public')))
+// http://localhost:3000/images/bg.jpeg
 
-app.use(express.static(path.resolve(__dirname + 'public')))
-// http://localhost:3000/images/bg.png
-
-app.use('/static', express.static(path.resolve(__dirname + 'public')))
-// http://localhost:3000/static/images/bg.png
+app.use('/static', express.static(path.resolve(__dirname + '/public')))
+// http://localhost:3000/static/images/bg.jpeg
 
 
 app.listen(3000, () => {
